4c4a627d2a6076df5c20b21d1bd49d1310ce2b18
AustineDavid <davmesoaustine@gmail.com>
2024-05-01 07:47:51 +0100

    worked on creating teachers, creating student for school and also creating classes

--- app/api/(school actions)/add-teacher-by-school/route.ts
+++ app/api/(school actions)/add-teacher-by-school/route.ts
@@ -1,3 +1,136 @@
-export async function GET() {
-  return new Response(JSON.stringify({ message: "working well" }));
+// in this route, we will be able to add a new teacher to the school
+// using the teachers id
+
+import prisma from "@/prisma/prismaConnect";
+
+// post request to add new teachers
+export async function POST(req: Request) {
+  // TODO: replace this school id here, and make use of next auth provided id,
+  // we can then pass only the teachers id through the body
+  const { teacherId, schoolId } = await req.json();
+  // return an error if both the id is not present
+  if (!teacherId || !schoolId) {
+    return new Response(JSON.stringify({ message: "teachers id required" }), {
+      status: 404,
+      statusText: "teacher id is required",
+    });
+  }
+  // we also check if the teacher was created before by the school
+  // we will return an error message showing that the teacher can not be created twice
+  const checkTeacherExistence = await prisma.schoolTeacher.findFirst({
+    where: {
+      schoolId,
+      teacherId,
+    },
+  });
+  if (checkTeacherExistence) {
+    return new Response(JSON.stringify({ message: "Teacher already exists" }), {
+      status: 404,
+    });
+  }
+  // lets proceed to creating the schoolTeacher for this particular teacher
+  try {
+    await prisma.schoolTeacher.create({
+      data: {
+        schoolId: schoolId,
+        teacherId: teacherId,
+      },
+    });
+    return new Response(
+      JSON.stringify({ message: "Teacher created successfully" }),
+      { status: 200, statusText: "success" }
+    );
+  } catch (error) {
+    throw new Error(JSON.stringify({ mesage: "server error" }));
+  }
+}
+
+// creating an update request
+// this request will be open for teachers to update
+// because is open for teachers to either accept or reject the offer
+export async function PUT(req: Request) {
+  // TODO: make use of the nextauth teacher id in this place instead of the teachersId passed in the body
+  const { teacherId, offerId, status } = await req.json();
+
+  // lets check if the id exist in the body
+  if (!teacherId || !offerId) {
+    return new Response(JSON.stringify({ message: "offer id not found" }), {
+      status: 404,
+      statusText: "offerid needed",
+    });
+  }
+  // lets first fetch the schoolTeacher that has this id
+  // check if the teacher id matches with the teacherId passed
+  const getSchoolTeacher = await prisma.schoolTeacher.findUnique({
+    where: {
+      id: offerId,
+    },
+  });
+  if (getSchoolTeacher?.teacherId !== teacherId) {
+    return new Response(
+      JSON.stringify({ message: "you can not modify this offer" }),
+      { status: 402 }
+    );
+  }
+  // now we can now modify the model
+  // that is probably changing it from pending to active or cancelled
+  try {
+    await prisma.schoolTeacher.update({
+      where: {
+        id: offerId,
+      },
+      data: {
+        status: status,
+      },
+    });
+    return new Response(JSON.stringify({ message: `you are now ${status}` }), {
+      status: 200,
+    });
+  } catch (error) {
+    throw new Error(
+      JSON.stringify({ message: "something went wrong try again later" })
+    );
+  }
+}
+
+// here, we should be able to delete teachers offers
+// is only the school that created this offer can delete it
+export async function DELETE(req: Request) {
+  // TODO: remember to change the schoolId from the body and make use of nextauth id for school
+  const { schoolId, offerId } = await req.json();
+  if (!offerId) {
+    return new Response(JSON.stringify({ message: "all input is needed" }), {
+      status: 404,
+    });
+  }
+  // lets first fetch the offer then compare if it is actually the school that created them
+  // if it is not, we return an error
+  // else we will proceed
+  const getOffer = await prisma.schoolTeacher.findUnique({
+    where: {
+      id: offerId,
+    },
+  });
+  if (getOffer?.schoolId !== schoolId) {
+    return new Response(
+      JSON.stringify({
+        message: "You do not have the power to delete this teacher",
+      }),
+      { status: 400 }
+    );
+  }
+  // now lets delete the offer/student
+  try {
+    await prisma.schoolTeacher.delete({
+      where: {
+        id: offerId,
+      },
+    });
+    return new Response(
+      JSON.stringify({ message: "teacher offer deleted successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    throw new Error(JSON.stringify({ message: "something went wrong" }));
+  }
 }
---
+++ app/api/(school actions)/class-action/route.ts
@@ -0,0 +1,134 @@
+// here we can create classes for school
+// using our post request
+// then later we can modify the or update the class when we want to add students or teachers to the platform
+import prisma from "@/prisma/prismaConnect";
+import Error from "next/error";
+
+// lets create class
+export async function POST(req: Request) {
+  // TODO: REMEMBER TO CHANGE THE SCHOOLID FROM THIS BODY AND USE NEXTAUTH ID
+  const { schoolId, ...others } = await req.json();
+  if (others) {
+    return new Response(JSON.stringify({ message: "inputs needed" }), {
+      status: 400,
+    });
+  }
+
+  // proceed to create
+  try {
+    await prisma.schoolClass.create({
+      data: {
+        ...others,
+        schoolId,
+      },
+    });
+    return new Response(
+      JSON.stringify({ message: "class created successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response("something went wrong", { status: 500 });
+  }
+}
+
+// here, lets update the class
+// this is done when the school management want to update the class information
+export async function PUT(req: Request) {
+  // TODO: CHANGE THE SCHOOLID present here to nextauth id
+  const { schoolId, classId, ...others } = await req.json();
+  if (!classId) {
+    return new Response(
+      JSON.stringify({ message: "class id is missing here" }),
+      { status: 400 }
+    );
+  }
+  // here, let's fetch the class and compare if is actually the school that wants to modify it
+  const getClass = await prisma.schoolClass.findUnique({
+    where: {
+      id: classId,
+    },
+  });
+  if (getClass?.schoolId !== schoolId) {
+    return new Response(
+      JSON.stringify({
+        message: "you can only update the class your school created",
+      }),
+      { status: 404 }
+    );
+  }
+  // proceed to update the class here
+  try {
+    await prisma.schoolClass.update({
+      where: {
+        id: schoolId,
+      },
+      data: {
+        ...others,
+      },
+    });
+    return new Response(
+      JSON.stringify({ message: "class has been updated successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response("something went wrong", { status: 500 });
+  }
+}
+
+// here, school will be able to delete a class
+// when the class is deleted, teachers and students associated with the class account should be removed
+// this is so that the students or teacher will not be seeing this classes
+export async function DELETE(req: Request) {
+  // TODO: REPLACE THE SCHOOLID WITH THE ID FROM NEXTAUTH
+  const { schoolId, classId } = await req.json();
+  if (!classId) {
+    return new Response(JSON.stringify({ message: "classId is required" }), {
+      status: 404,
+    });
+  }
+  // here, let's fetch the class and compare if is actually the school that wants to modify it
+  const getClass = await prisma.schoolClass.findUnique({
+    where: {
+      id: classId,
+    },
+    include: {
+      SchoolClassStudent: true,
+      SchoolClassTeacher: true,
+    },
+  });
+  //   if the class does not exist, return an error
+  if (!getClass) {
+    return new Response(JSON.stringify({ message: "class not found" }), {
+      status: 404,
+    });
+  }
+  if (getClass?.schoolId !== schoolId) {
+    return new Response(
+      JSON.stringify({
+        message: "you can only delete the class your school created",
+      }),
+      { status: 404 }
+    );
+  }
+  //   now, lets try deleting the class, teacher and student
+  try {
+    // first delete all the student associated with the class
+    for (const student of getClass.SchoolClassStudent) {
+      await prisma.schoolClassStudent.delete({ where: { id: student.id } });
+    }
+    // lets now delete all the teachers associated with the class
+    for (const teacher of getClass.SchoolClassTeacher) {
+      await prisma.schoolClassTeacher.delete({ where: { id: teacher.id } });
+    }
+    // now we can delete the class itself
+    await prisma.schoolClass.delete({ where: { id: classId } });
+    return new Response(
+      JSON.stringify({ message: "class deleted successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ message: "something went wrong" }), {
+      status: 500,
+    });
+  }
+}
