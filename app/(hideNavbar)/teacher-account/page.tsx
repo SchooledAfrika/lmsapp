3ccad2a01374595ad2c3b68856a6a0fcd448e799
AustineDavid <davmesoaustine@gmail.com>
2024-05-12 15:00:27 +0100

    making some important correction for build to happen

--- app/(hideNavbar)/teacher-account/page.tsx
+++ app/(hideNavbar)/teacher-account/page.tsx
@@ -1,1 +1,8 @@
 // Teachers will be redirected to this route when they choose to register as teachers to complete their registration
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
--- components/ChartDetails.tsx
+++ components/ChartDetails.tsx
@@ -1,54 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const ChartDetails = () => {
   return (
     <div>
-        <div className='flex justify-between my-3'>
-            <div>
-                <h3 className='text-lightGreen font-bold'>70% - 100%</h3>
-                <p className=''><span className='text-lightGreen font-semibold'>15%</span> more than last month's average</p>
-            </div>
-            <div className='font-bold'>
-                <h3 className=''>5 Students</h3>
-                <p>Monthly average</p>
-            </div>
+      <div className="flex justify-between my-3">
+        <div>
+          <h3 className="text-lightGreen font-bold">70% - 100%</h3>
+          <p className="">
+            <span className="text-lightGreen font-semibold">15%</span> more than
+            last month&apos;s average
+          </p>
         </div>
+        <div className="font-bold">
+          <h3 className="">5 Students</h3>
+          <p>Monthly average</p>
+        </div>
+      </div>
 
-        <div className='flex justify-between my-3'>
-            <div>
-                <h3 className='text-rose-400 font-bold'>50% - 69%</h3>
-                <p className=''><span className='text-lightGreen font-semibold'>15%</span> more than last month's average</p>
-            </div>
-            <div className='font-bold'>
-                <h3 className=''>7 Students</h3>
-                <p>Monthly average</p>
-            </div>
+      <div className="flex justify-between my-3">
+        <div>
+          <h3 className="text-rose-400 font-bold">50% - 69%</h3>
+          <p className="">
+            <span className="text-lightGreen font-semibold">15%</span> more than
+            last month&apos;s average
+          </p>
         </div>
+        <div className="font-bold">
+          <h3 className="">7 Students</h3>
+          <p>Monthly average</p>
+        </div>
+      </div>
 
-        <div className='flex justify-between my-3'>
-            <div>
-                <h3 className='text-dimYellow font-bold'>40% - 49%</h3>
-                <p className=''><span className='text-lightGreen font-semibold'>15%</span> more than last month's average</p>
-            </div>
-            <div className='font-bold'>
-                <h3 className=''>8 Students</h3>
-                <p>Monthly average</p>
-            </div>
+      <div className="flex justify-between my-3">
+        <div>
+          <h3 className="text-dimYellow font-bold">40% - 49%</h3>
+          <p className="">
+            <span className="text-lightGreen font-semibold">15%</span> more than
+            last month&apos;s average
+          </p>
         </div>
+        <div className="font-bold">
+          <h3 className="">8 Students</h3>
+          <p>Monthly average</p>
+        </div>
+      </div>
 
-        <div className='flex justify-between my-3'>
-            <div>
-                <h3 className='text-red-600 font-bold'>0% - 39%</h3>
-                <p className=''><span className='text-lightGreen font-semibold'>15%</span> more than last month's average</p>
-            </div>
-            <div className='font-bold'>
-                <h3 className=''>4 Students</h3>
-                <p>Monthly average</p>
-            </div>
+      <div className="flex justify-between my-3">
+        <div>
+          <h3 className="text-red-600 font-bold">0% - 39%</h3>
+          <p className="">
+            <span className="text-lightGreen font-semibold">15%</span> more than
+            last month&apos;s average
+          </p>
         </div>
-       
+        <div className="font-bold">
+          <h3 className="">4 Students</h3>
+          <p>Monthly average</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChartDetails
+export default ChartDetails;
