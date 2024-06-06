2c891e116bcbb611893762538742eccdd64477ec
Pricelesssarah-ui <pricelesssarah3@gmail.com>
2024-06-07 00:11:19 +0100

    Created the teachers authentication pages

--- components/TeacherAccount.tsx
+++ components/TeacherAccount.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import TeacherInfo from "./ui/teacher-login/TeacherInfo";
+import TeacherResume from "./ui/teacher-login/TeacherResume";
+import TeacherPaymentDetails from "./ui/teacher-login/TeacherPaymentDetails";
+import TeacherFinalPaymentDetails from "./ui/teacher-login/TeacherFinalPaymentDetails";
 
 const TeacherAccount = () => {
-  return <div>TeacherAccount</div>;
+  const [showComponent, setShowComponent] = useState("Personal Information");
+
+  const handleComponentView = (view: any) => {
+    setShowComponent(view);
+  };
+
+  return (
+    <div>
+      {showComponent === "Personal Information" && (
+        <TeacherInfo onClickButton={handleComponentView} />
+      )}
+      {showComponent === "Resume" && (
+        <TeacherResume onClickButton={handleComponentView} />
+      )}
+      {showComponent === "Payment Details" && (
+        <TeacherPaymentDetails onClickButton={handleComponentView} />
+      )}
+      {showComponent === "Final Payment Details" && (
+        <TeacherFinalPaymentDetails />
+      )}
+    </div>
+  );
 };
 
 export default TeacherAccount;
