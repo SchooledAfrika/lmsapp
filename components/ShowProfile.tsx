c99efa0d8c327fe9acf16070e399540730061751
AustineDavid <davmesoaustine@gmail.com>
2024-07-09 11:34:27 +0100

    added show profile image in the home page

---
+++ components/ShowProfile.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { RxAvatar } from "react-icons/rx";
+import { useRouter } from "next/navigation";
+
+const ShowProfile = () => {
+  const { data } = useSession();
+  const router = useRouter();
+
+  const handleToDashboard = () => {
+    if (data?.user.role === "Teacher") {
+      router.push("/teacher-dashboard");
+    } else if (data?.user.role === "Student") {
+      router.push("/student-dashboard");
+    } else if (data?.user.role === "admin") {
+      router.push("/admin-dashboard");
+    } else if (data?.user.role === "School") {
+      router.push("/school-dashboard");
+    } else {
+      router.push("/parents-dashboard");
+    }
+  };
+  return (
+    <div
+      onClick={handleToDashboard}
+      className=" flex items-center gap-2 cursor-pointer mr-8"
+    >
+      <p className=" text-green-700 text-[12px] font-bold">My Dashboard</p>
+      <div className=" p-1  w-[40px] h-[40px] border border-green-700 rounded-full">
+        {data?.user.image ? (
+          <Image
+            className=" w-full h-full object-cover rounded-full"
+            priority
+            src={data.user.image}
+            alt="profile"
+            width={200}
+            height={200}
+          />
+        ) : (
+          <RxAvatar className="w-full h-full" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ShowProfile;
