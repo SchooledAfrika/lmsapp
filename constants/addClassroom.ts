d7cbf9602d5910b542d00688302e0c9b1d95eb92
AustineDavid <davmesoaustine@gmail.com>
2024-07-30 09:01:38 +0100

    updated upload link

--- constants/addClassroom.ts
+++ constants/addClassroom.ts
@@ -77,3 +77,6 @@ export const addingClassroomSchema = z.object({
   maxCapacity: z.string({ message: "enter your class capacity" }),
   classTime: z.string().min(3, { message: "field is required" }),
 });
+
+export const urlRegex =
+  /^(https?:\/\/)?((([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|\d{1,3}(\.\d{1,3}){3})(:\d+)?(\/[-a-zA-Z\d%_.~+]*)*(\?[;&a-zA-Z\d%_.~+=-]*)?(#[a-zA-Z\d_]*)?$/;
