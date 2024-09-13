c504d37cc6e7dce8fd81710b72339758999ec5f0
Odo Maurice <odomaurice@yahoo.com>
2024-09-13 18:26:19 +0100

    initialized work on admin querying teachers

--- app/(hideNavbar)/teacher-dashboard/calender/page.tsx
+++ app/(hideNavbar)/teacher-dashboard/calender/page.tsx
@@ -1,7 +1,10 @@
+import Calendar from "@/components/ui/teacher-dashboard/calendar/Calendar";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <div className="mt-[80px]">
+    <Calendar/>
+  </div>;
 };
 
 export default page;
