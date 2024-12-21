fdb892b33091025c2f3dd1f3636ce50c6c22a91a
AustineDavid <davmesoaustine@gmail.com>
2024-12-21 05:12:09 +0100

    solving the error in teacher getting the vacancies

--- app/api/(teacher actions)/teachers-offer/route.ts
+++ app/api/(teacher actions)/teachers-offer/route.ts
@@ -2,3 +2,40 @@
 // they can also be able to delete their applications
 
 import prisma from "@/prisma/prismaConnect";
+import { notAuthenticated, serverError } from "@/prisma/utils/error";
+import { serverSessionId, serverSessionRole } from "@/prisma/utils/utils";
+
+export async function GET(req: Request) {
+  // check for authentication
+  const teacherId = await serverSessionId();
+  const role = await serverSessionRole();
+  if (!teacherId) return notAuthenticated();
+  if (role !== "Teacher") {
+    return new Response(
+      JSON.stringify({ message: "only teachers can access this..." }),
+      { status: 400 }
+    );
+  }
+  //   proceed to getting information
+  try {
+    const openOffers = await prisma.vacancyTeacher.findMany({
+      where: { teacherId, status: "PENDING" },
+      include: {
+        vacancy: {
+          select: {
+            role: true,
+            description: true,
+            qualifications: true,
+            responsibility: true,
+            minSalary: true,
+            maxSalary: true,
+            note: true,
+          },
+        },
+      },
+    });
+    return new Response(JSON.stringify(openOffers), { status: 200 });
+  } catch (error) {
+    return serverError();
+  }
+}
