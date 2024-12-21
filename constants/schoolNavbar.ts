11291b6d177f18f960606c9bf4393515aed3f7d2
AustineDavid <davmesoaustine@gmail.com>
2024-12-21 04:37:59 +0100

    finally removed the whole school dashboard route

--- constants/schoolNavbar.ts
+++ constants/schoolNavbar.ts
@@ -10,55 +10,6 @@ export interface SchoolNavType {
   subDetails?: Subtype;
 }
 
-export const SchoolNavbar: SchoolNavType[] = [
-  {
-    title: "Welcome to Your School Dashboard",
-    description: "Manage your school at a go! Just a click!",
-    path: "overview",
-  },
-  {
-    title: "Classroom",
-    description: "Get your classroom ready",
-    path: "classroom",
-    subDetails: {
-      title: " Single class here",
-      description: " this is for single class path",
-    },
-  },
-  {
-    title: "Teachers",
-    description: "Organize your teachers and review their data",
-    path: "teachers",
-    subDetails: {
-      title: " Single teacher here",
-      description: " this is for single teacher path",
-    },
-  },
-  {
-    title: "Students",
-    description: "Organize your students and review their data",
-    path: "students",
-    subDetails: {
-      title: " Single student here",
-      description: " this is for single student path",
-    },
-  },
-  {
-    title: "Calendar",
-    description: "View schedules of your teachers and students",
-    path: "calender",
-  },
-  {
-    title: "Job Listing",
-    description: "View, update and delete jobs here.",
-    path: "job-listing",
-  },
-  {
-    title: "Settings",
-    description: "Edit your settings here",
-    path: "settings",
-  },
-];
 export const TeacherNavbar: SchoolNavType[] = [
   {
     title: "Welcome to Your Dashboard",
