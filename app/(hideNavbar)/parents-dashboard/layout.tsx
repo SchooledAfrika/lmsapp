55687407952a733b1973089e34f16b8fcbb0819d
AustineDavid <davmesoaustine@gmail.com>
2025-01-07 14:17:26 +0100

    loading error on checking

--- app/(hideNavbar)/parents-dashboard/layout.tsx
+++ app/(hideNavbar)/parents-dashboard/layout.tsx
@@ -30,7 +30,6 @@ export default async function RootLayout({
   // now, lets check if the ward is selected before visiting any of this page
   const cookieStore = cookies;
   const wardId = cookieStore().get("wardId")?.value;
-  console.log("the wardId", wardId);
   if (!wardId) return redirect("/parent-account/ward-options");
   return (
     <>
