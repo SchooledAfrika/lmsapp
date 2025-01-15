f1c7e931ef976bbf3132a232f493d222f106fd18
Odo Maurice <odomaurice@yahoo.com>
2025-01-15 14:24:44 +0100

    worked on teacher adding test and resources to a particular student in sessions and modified the singleSession UI

---
+++ app/(hideNavbar)/student-dashboard/sessions/special-request/[id]/page.tsx
@@ -0,0 +1,12 @@
+import SingleSpecialSessionShow from "@/components/SingleSpecialSessionShow";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className=" pt-4">
+      <SingleSpecialSessionShow isTeacher={false} />
+    </div>
+  );
+};
+
+export default page;
